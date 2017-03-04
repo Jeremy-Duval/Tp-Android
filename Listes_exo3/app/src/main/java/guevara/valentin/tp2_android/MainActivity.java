552c1a08b1787ecb703d002077813ea1841a17ec
Jeremy Duval <jeremy-duval1@hotmail.fr>
2017-03-04 16:06:22 +0100

    Copy Past the ex2 in the 3 to create the list (in mainActivity)

--- Listes_exo3/app/src/main/java/guevara/valentin/tp2_android/MainActivity.java
+++ Listes_exo3/app/src/main/java/guevara/valentin/tp2_android/MainActivity.java
@@ -1,25 +1,84 @@
 package guevara.valentin.tp2_android;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.app.LauncherActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.RadioButton;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
-
-import static guevara.valentin.tp2_android.R.styleable.View;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView musique;
+    private ArrayList<HashMap<String,String>> listItem;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //fetch the listView created in the XML file Main
+        musique = (ListView) findViewById(R.id.musique);
+
+        //HashMap with all elements (Image,Description...) inside the list
+        listItem = new ArrayList<>();
+
+        //We declare an Hashmap which is equivalent to one element
+        HashMap<String,String> element = new HashMap<>();
+
+        //We have to make these operations a lot of times for creating another element
+        element = new HashMap<>();
+        element.put("titre", "Membre du G2S4 et développeur de choc !");
+        element.put("description", "Valentin Guevara");
+        element.put("image", String.valueOf(R.mipmap.ic_default_profile));
+        listItem.add(element);
+
+        element = new HashMap<>();
+        element.put("titre", "Membre du G2S4 et développeur de choc !");
+        element.put("description", "Jérémy Duval");
+        element.put("image", String.valueOf(R.mipmap.ic_default_profile));
+        listItem.add(element);
+
+        element = new HashMap<>();
+        element.put("titre", "Vache volante, idole de tout les peuples");
+        element.put("description", "Marguerite Cow");
+        element.put("image", String.valueOf(R.mipmap.ic_default_profile));
+        listItem.add(element);
+
+        element = new HashMap<>();
+        element.put("titre", "Connue de tout les programmeur mais personne ne sait qui il est vraiment");
+        element.put("description", "Toto TitiTataTutu");
+        element.put("image", String.valueOf(R.mipmap.ic_default_profile));
+        listItem.add(element);
+
+        //creation of a SimpleAdapter
+        SimpleAdapter musique_schedule = new SimpleAdapter(this.getBaseContext(), listItem, R.layout.affichageitem,
+                new String[]{"image", "titre", "description"},
+                new int[]{R.id.img, R.id.titre, R.id.description}
+
+        );
+
+        musique.setAdapter(musique_schedule);
+
+        musique.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                HashMap<String, String> hashmapselected = (HashMap<String, String>) musique.getItemAtPosition(position);
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Music selected");
+                alert.setIcon(Integer.parseInt(hashmapselected.get("image")));
+                alert.setMessage("You have selected : " + hashmapselected.get("titre").toString() + " by " + hashmapselected.get("description").toString());
+                alert.setPositiveButton("Got It", null);
+                alert.show();
+            }
+        });
+
+
+    }
 }
