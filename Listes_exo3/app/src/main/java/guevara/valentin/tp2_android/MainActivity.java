8df266d7aa65e103c21a01ee4b2d9960f1a5a583
Jeremy Duval <jeremy-duval1@hotmail.fr>
2017-03-04 15:52:27 +0100

    Ex3 : Transfer of main activity in activityA (form) :
    Main will become list

---
+++ Listes_exo3/app/src/main/java/guevara/valentin/tp2_android/MainActivity.java
@@ -0,0 +1,25 @@
+package guevara.valentin.tp2_android;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import static guevara.valentin.tp2_android.R.styleable.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
