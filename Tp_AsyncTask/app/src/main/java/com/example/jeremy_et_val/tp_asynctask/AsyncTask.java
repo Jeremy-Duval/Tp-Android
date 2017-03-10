b09b4c7ebd6f8c076a9e18067782b873f8ce0e75
Jeremy Duval <jeremy-duval1@hotmail.fr>
2017-03-10 17:25:25 +0100

    XML

---
+++ Tp_AsyncTask/app/src/main/java/com/example/jeremy_et_val/tp_asynctask/AsyncTask.java
@@ -0,0 +1,12 @@
+package com.example.jeremy_et_val.tp_asynctask;
+
+/**
+ * Created by jeremy on 10/03/17.
+ */
+
+public class AsyncTask extends android.os.AsyncTask<Object,Void,String> {
+    @Override
+    protected String doInBackground(Object... params) {
+        return null;
+    }
+}
