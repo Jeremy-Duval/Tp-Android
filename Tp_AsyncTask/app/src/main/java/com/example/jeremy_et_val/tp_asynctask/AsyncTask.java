053058935329b53d8b53e807b13ef4bb1bdac512
Jeremy Duval <jeremy-duval1@hotmail.fr>
2017-03-17 08:39:14 +0100

    refactor xml name and webView

--- Tp_AsyncTask/app/src/main/java/com/example/jeremy_et_val/tp_asynctask/AsyncTask.java
+++ Tp_AsyncTask/app/src/main/java/com/example/jeremy_et_val/tp_asynctask/AsyncTask.java
@@ -1,12 +1,21 @@
 package com.example.jeremy_et_val.tp_asynctask;
 
+import android.widget.TextView;
+
 /**
  * Created by jeremy on 10/03/17.
  */
 
 public class AsyncTask extends android.os.AsyncTask<Object,Void,String> {
+    String chaine;
+    TextView tv;
+
     @Override
     protected String doInBackground(Object... params) {
+        chaine = (String) params[0];
+        tv = (TextView) params[1];
+        // connexions http, traitements lourds, etc.
+
         return null;
     }
 }
--- Tp_AsyncTask/app/src/main/java/com/example/jeremy_et_val/tp_asynctask/MainActivity.java
+++ Tp_AsyncTask/app/src/main/java/com/example/jeremy_et_val/tp_asynctask/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.jeremy_et_val.tp_asynctask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView_google;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView_google = (WebView) findViewById(R.id.webview_google);
     }
+
+
 }
+
